7ad397eb6189e13750dfd325bb5dba37ea8ec69d
MarceloDiasst <marcelo.diasst@gmail.com>
2024-01-16 15:04:31 -0300

    primeiro commit

---
+++ Projeto Front/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+     <div className="Container">
+          <h1>Helo boostrap </h1>
+         <div className="btn bt -outline-primary">Cadastrar</div>
+         <div className="btn bt -outline-primary">Listar</div>
+     </div>
+    </>
+  )
+}
+
+export default App
